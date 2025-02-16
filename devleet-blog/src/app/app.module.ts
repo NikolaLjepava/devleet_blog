8886e86f59da881ce3acee0999419adf0238c72b
Nikola <nikola.ljepava@gmail.com>
2025-02-16 20:54:20 +0100

    some issue fixed after merge conflicts

--- devleet-blog/src/app/app.module.ts
+++ devleet-blog/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
-import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BlogListComponent } from './components/blog-list/blog-list.component';
+import { CreateBlogComponent } from './components/create-blog/create-blog.component';
+import { UpdateBlogComponent } from './components/update-blog/update-blog.component';
+import { DeleteBlogComponent } from './components/delete-blog/delete-blog.component';
+import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './services/auth.service';
+import { BlogService } from './services/blog.service';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  // Import BrowserAnimationsModule
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,16 +23,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
     HomeComponent,
     SignupComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    BlogListComponent,
+    CreateBlogComponent,
+    UpdateBlogComponent,
+    DeleteBlogComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule,
     MatSnackBarModule,
-    BrowserAnimationsModule // Add BrowserAnimationsModule here
+    BrowserAnimationsModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, BlogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
